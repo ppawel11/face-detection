f46d433f136ebc5cd993e1ba1a4f281ad2c7711b
KDKaczkowski <49338595+KDKaczkowski@users.noreply.github.com>
2020-06-12 23:05:44 +0200

    Add files via upload
    Test wydajności 2.7

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include "Connection/Connection.h"
+#include "Detection/Detector.h"
+
+int main() {
+    Connection connection = Connection();
+    Detector detector = Detector();
+
+    cv::Mat pic;
+    try{
+        connection.openReceivingMQ();
+        connection.createSendingMQ();
+    }catch(std::runtime_error & e){
+        std::cout<<e.what()<<std::endl;
+        return 1;
+    }
+
+    while(true){
+        try{
+            pic = connection.read();
+        }catch(std::runtime_error & e){
+            std::cout<<e.what()<<std::endl;
+            break;
+        }
+	for(int i =0; i < 10; i++){
+            detector.detectAndMarkFaces(pic);
+	}
+        Message msg(pic);
+        connection.sendData(msg);
+//        imshow("detected", pic);
+        //if( cv::waitKey(10) == 27 ) break; // ESC to quit
+    }
+
+    connection.closeReceivingMQ();
+    connection.deleteSendingMQ();
+
+    return 0;
+}
